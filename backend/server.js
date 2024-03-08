d0097c0e0981137114966b38ecf59d695ad21249
h_ri <h_ri@satt.jp>
2024-03-08 09:19:45 +0900

    error

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import { connectToMongoDB } from "./db/connectToMongoDB.js";
-import {app} from "./socket/socket.js"
+import {app,server} from "./socket/socket.js"
 
 dotenv.config();
 const PORT = 8000 || process.env.PORT || 5000;
